ae146e4898701582677919be9a388906cbc0d11d
Harrison <hmcguire13@gmail.com>
2014-11-17 15:12:31 -0500

    Initial Commit

---
+++ ReadFacebook/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ReadFacebook
+//
+//  Created by Harrison McGuire on 11/17/14.
+//  Copyright (c) 2014 harrisonmcguire. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
